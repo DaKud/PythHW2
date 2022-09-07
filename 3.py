1118634bd92dec42005d23a1e99638706625abb9
DaKu <do.holyada@gmail.com>
2022-09-07 22:04:20 +0300

    third

--- 3.py
+++ 3.py
@@ -1,1 +1,11 @@
-# Задайте список из n чисел последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
+# Задайте список из n чисел последовательности 
+# (1+1/n)^n и выведите на экран их сумму.
+
+number = int(input('Insert number:' ))
+result = list()
+
+sum = 0
+for i in range(1, number+1):
+    sum = int(sum + round((1+1/i) ** i, 0))
+    result.append(sum)
+print(f'n = {number} and as  result you will get {list(enumerate(result,))}')
